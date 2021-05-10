af7e16ebae6993534993adfe402c537b6e481ae8
marioguidos <82772404+marioguidos@users.noreply.github.com>
2021-05-10 01:58:16 -0600

    cambio

--- app/src/main/java/com/example/parcial2/ShowPromedio.java
+++ app/src/main/java/com/example/parcial2/ShowPromedio.java
@@ -10,5 +10,6 @@ public class ShowPromedio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_promedio);
+
     }
 }
